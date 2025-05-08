5e365198ead4ccf0d1c19723584fe3a8de06cd6a
haizhou-zhao <zhaohaizhou940527@gmail.com>
2025-05-08 16:46:56 -0700

    [Red: Parser] Parsing create tbl stmt

--- red/public/SqlPlan.h
+++ red/public/SqlPlan.h
@@ -1,10 +1,35 @@
 #pragma once
 
-class CreateTable
+#include <string>
+#include <vector>
+
+struct Identifier
 {
-private:
-    /* data */
-public:
-    CreateTable(/* args */);
-    ~CreateTable();
+    std::vector<std::string> nameParts;
+};
+
+struct DataType
+{
+    std::string typeName;
+};
+
+struct ColumnDef
+{
+    std::string name;
+    DataType dataType;
+    bool bNullable;
+    std::string comment;
+    // DefaultValueExpression defaultValue;
+    // std::string generationExpression;
+    // IdentityColumnSpec identityColumnSpec;
+    // Metadata metadata;
+};
+
+struct CreateTable
+{
+    Identifier name;
+    std::vector<ColumnDef> columns;
+    // std::vector<Transform> partitioning;
+    // TableSpec tableSpec;
+    bool bIfNotExist;
 };
