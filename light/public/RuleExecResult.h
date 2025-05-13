94f55329a80dbb84e83f0363efddb058eb2ab416
haizhou-zhao <zhaohaizhou940527@gmail.com>
2025-05-13 14:40:40 -0700

    [Light: Common] Backfill missing files

---
+++ light/public/RuleExecResult.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <memory>
+
+class TreeNode;
+
+struct RuleExecResult
+{
+    std::shared_ptr<TreeNode> afterRule;
+    bool bChanged;
+
+    RuleExecResult(std::shared_ptr<TreeNode> _afterRule, bool _bChanged) : afterRule(_afterRule), bChanged(_bChanged) {}
+};
