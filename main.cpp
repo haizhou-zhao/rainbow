c285adbf25ee6ac100a877c313640c9f3a1e4595
haizhou-zhao <zhaohaizhou940527@gmail.com>
2025-06-11 23:46:38 -0700

    [Red: Parser] Support Simple Insert Into Table

--- main.cpp
+++ main.cpp
@@ -25,8 +25,6 @@ int main(int , const char **) {
 
   SqlBaseParser parser(&tokens);
   SqlBaseParser::CompoundOrSingleStatementContext* parsed = parser.compoundOrSingleStatement();
-
-  std::cout << parsed->toStringTree() << std::endl;
   std::shared_ptr<CreateTable> plan = std::any_cast<std::shared_ptr<CreateTable>>(AstBuilder().visitCompoundOrSingleStatement(parsed));
   ResolveCatalogs r;
   r.init(&sqlCtx);
@@ -39,11 +37,17 @@ int main(int , const char **) {
   CommonTokenStream tokens2(&lexer2);
   SqlBaseParser parser2(&tokens2);
   SqlBaseParser::CompoundOrSingleStatementContext* parsed2 = parser2.compoundOrSingleStatement();
-  std::cout << parsed2->toStringTree() << std::endl;
   std::shared_ptr<AddColumns> plan2 = std::any_cast<std::shared_ptr<AddColumns>>(AstBuilder().visitCompoundOrSingleStatement(parsed2));
   auto result2 = plan2->resolveRulesDownWithPruning([](const TreeNode* n){ return false; }, &r);
   (std::dynamic_pointer_cast<AddColumns>(result2.afterRule))->run(&sqlCtx);
   assert(sqlCtx.currentCatalog()->getTable(sqlCtx.currentDatabaseName(), "my_tbl1")->schema.size() == 3);
+
+  ANTLRInputStream input3("INSERT INTO my_tbl1 VALUES (1, 'data1', '2023-10-01 12:00:00')");
+  SqlBaseLexer lexer3(&input3);
+  CommonTokenStream tokens3(&lexer3);
+  SqlBaseParser parser3(&tokens3);
+  SqlBaseParser::CompoundOrSingleStatementContext* parsed3 = parser3.compoundOrSingleStatement();
+  std::shared_ptr<InsertInto> plan3 = std::any_cast<std::shared_ptr<InsertInto>>(AstBuilder().visitCompoundOrSingleStatement(parsed3));
 
   return 0;
 }
